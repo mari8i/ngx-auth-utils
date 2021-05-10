d5da0467f6057cf3256a24f13010c371dc086392
Alessandro Mariotti <alessandro.mariotti@zupit.it>
2021-05-10 18:46:09 +0200

    On my way to adding guard events

--- projects/ngx-auth-utils/src/lib/guards/anon-user.guard.ts
+++ projects/ngx-auth-utils/src/lib/guards/anon-user.guard.ts
@@ -1,5 +1,5 @@
 import { Inject, Injectable } from '@angular/core';
-import { CanActivate, Router, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { map } from 'rxjs/operators';
 import { AuthenticationService } from '../services/authentication.service';
 import { HOME_URL } from '../config';
@@ -11,10 +11,11 @@ import { Observable } from 'rxjs';
 export class AnonUserGuard implements CanActivate {
     constructor(private authenticationService: AuthenticationService, private router: Router, @Inject(HOME_URL) private homeUrl: string) {}
 
-    canActivate(): Observable<boolean | UrlTree> {
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> {
         return this.authenticationService.getAuthenticationState().pipe(
             map((identity) => {
                 if (identity != null) {
+                    this.authenticationService.notifyGuardBlockedAccess('AnonUserGuard', route, state);
                     return this.router.parseUrl(this.homeUrl);
                 }
                 return true;
--- projects/ngx-auth-utils/src/lib/guards/auth-user.guard.spec.ts
+++ projects/ngx-auth-utils/src/lib/guards/auth-user.guard.spec.ts
@@ -1,4 +1,4 @@
-import { Router, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable, of } from 'rxjs';
 import { AuthenticationService } from '../services/authentication.service';
 import { AuthUserGuard } from './auth-user.guard';
@@ -13,10 +13,13 @@ describe('AuthUserPredicateGuard', () => {
             routerSpy = jasmine.createSpyObj<Router>('Router', ['navigate', 'parseUrl']);
             guard = new AuthUserGuard(serviceStub as AuthenticationService, routerSpy, undefined);
             serviceStub.getAuthenticationState = (): Observable<any> => of(null);
+            serviceStub.notifyGuardBlockedAccess = (): void => {
+                return;
+            };
         });
 
         it('emits false when user is not authenticated', (done: DoneFn) => {
-            guard.canActivate().subscribe((res: UrlTree | boolean) => {
+            guard.canActivate({} as ActivatedRouteSnapshot, {} as RouterStateSnapshot).subscribe((res: UrlTree | boolean) => {
                 expect(res).toBeFalse();
                 done();
             });
@@ -28,13 +31,16 @@ describe('AuthUserPredicateGuard', () => {
             routerSpy = jasmine.createSpyObj<Router>('Router', ['navigate', 'parseUrl']);
             guard = new AuthUserGuard(serviceStub as AuthenticationService, routerSpy, '/error');
             serviceStub.getAuthenticationState = (): Observable<any> => of(null);
+            serviceStub.notifyGuardBlockedAccess = (): void => {
+                return;
+            };
         });
 
         it('redirects to configured URL when user is not authenticated', (done: DoneFn) => {
             const dummyUrlTree: UrlTree = new UrlTree();
             routerSpy.parseUrl.and.returnValue(dummyUrlTree);
 
-            guard.canActivate().subscribe((url: UrlTree | boolean) => {
+            guard.canActivate({} as ActivatedRouteSnapshot, {} as RouterStateSnapshot).subscribe((url: UrlTree | boolean) => {
                 expect(url).toBeInstanceOf(UrlTree);
                 expect(url).toEqual(dummyUrlTree);
                 expect(routerSpy.parseUrl.calls.mostRecent().args).toEqual(['/error']);
@@ -48,13 +54,17 @@ describe('AuthUserPredicateGuard', () => {
             routerSpy = jasmine.createSpyObj<Router>('Router', ['navigate', 'parseUrl']);
             guard = new AuthUserGuard(serviceStub as AuthenticationService, routerSpy, undefined);
             serviceStub.getAuthenticationState = (): Observable<any> => of({ username: 'foo' });
+            // TODO: Test this is called! Also elsewhere
+            serviceStub.notifyGuardBlockedAccess = (): void => {
+                return;
+            };
         });
 
         it('emits true when user is authenticated', (done: DoneFn) => {
             const dummyUrlTree: UrlTree = new UrlTree();
             routerSpy.parseUrl.and.returnValue(dummyUrlTree);
 
-            guard.canActivate().subscribe((res: UrlTree | boolean) => {
+            guard.canActivate({} as ActivatedRouteSnapshot, {} as RouterStateSnapshot).subscribe((res: UrlTree | boolean) => {
                 expect(res).toBeTrue();
                 done();
             });
